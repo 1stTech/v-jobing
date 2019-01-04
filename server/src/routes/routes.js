d8fd8552e0037810043c14f5aa70b64e264c32db
kuznets <sresetnjak@gmail.com>
2019-01-04 05:07:12 +0200

    Dug: If nodemon restart the project it return EADDRINUSE error.

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,11 +1,11 @@
 const router = require('express').Router()
 
-// const CreateCountriesMiddleware = require('../shared/middlewares/create-countries')
+const CreateCountriesMiddleware = require('../shared/middlewares/create-countries')
 
 router.get('/', (req, res) => {
   res.status(200).send('v-jobing OK!')
 })
 
-// router.get('/create-countries', CreateCountriesMiddleware.getAllCountries)
+router.get('/create-countries', CreateCountriesMiddleware.getAllCountries)
 
 module.exports = router
